aa81c450835e95e5f1746add841e18356dc91863
郭立平 <liping0_0@my.swjtu.edu.cn>
2021-07-26 09:43:01 +0800

    work with excel (using pandas)

--- work_with_excel.py
+++ work_with_excel.py
@@ -36,6 +36,32 @@ df.head()
 df.tail()
 df.shape()
 
+# Selection DataFrame
+df1 = pd.DataFrame(np.random.randn(6,4), index=list('abcdef'), columns=list('ABCD'))
+df1.loc[['a', 'b', 'd'], :]
+df1.loc['d':, 'A':'C']
+df1.loc['a']
+df1.loc['a'] > 0
+df1.loc[:, df1.loc['a'] > 0]
+df1.loc['a', 'A']
+df1.loc[lambda df: df.A > 0, :]
+df1.loc[:, lambda df: ['A', 'B']]
+df1.iloc[:, lambda df: [0, 1]]
+df1[lambda df: df.columns[0]]
+df1.A.loc[lambda s: s > 0]
+df2 = pd.DataFrame(np.random.randn(6,4), index=list(range(0,12,2)), columns=list(range(0,8,2)))
+df2.iloc[:3]
+df2.iloc[1:5, 2:4]
+df2.iloc[[1, 3, 5], [1, 3]]
+df2.iloc[1:3, :]
+df2.iloc[:, 1:3]
+df2.iloc[1, 1]
+
+# Convert to numpy array
+df = pd.DataFrame([[21, 72, 67], [23, 78, 69], [32, 74, 56], [52, 54, 76]],	columns=['a', 'b', 'c'])
+arr = df.to_numpy()  # Note that the recommended approach is df.to_numpy().
+arr = df.values
+
 # Get Statistics
 df.count() # Count Rows
 df.describe() # Get general statistics (min,max,mean,std,...)
